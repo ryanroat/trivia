744582aff28385260eda1801ed7330b33d219f09
Ryan Roat <ryanroat@gmail.com>
2020-09-21 19:57:22 -0400

    show question and answer from trivia API

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-const url = 'https://opentdb.com/api.php?amount=2';
+/* eslint-disable linebreak-style */
+const url = 'https://opentdb.com/api.php?amount=1';
+
+const question = document.querySelector('.question');
+const answerBtn = document.querySelector('.answerBtn');
+const answer = document.querySelector('.answer');
 
 // fetch(url)
 //     .then(response => response.json())
 //     .then(data => console.log(data));
 
 async function getTrivia() {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+function showAnswer(trivia) {
+  trivia.then((data) => {
+    console.log(data);
+    answer.innerHTML = data.results[0].correct_answer;
+  });
 }
 
 const trivia = getTrivia();
 // const trivia = async () => await getTrivia();
-trivia.then(data => console.log(data));
+trivia.then((data) => {
+  // console.log(data);
+  question.innerHTML = data.results[0].question;
+});
+
+answerBtn.addEventListener('click', showAnswer(trivia));
