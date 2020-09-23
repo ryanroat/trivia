608fa8d1d1d71eca64639fe40c9644daaff38b98
Ryan Roat <ryanroat@gmail.com>
2020-09-22 20:49:18 -0400

    display multiple choice options

--- main.js
+++ main.js
@@ -23,18 +23,29 @@ function showAnswer(trivia) {
 }
 
 function showQuestion(trivia) {
+  const option = document.querySelector('.option');
   trivia.then((data) => {
-    const query = data.results[0];
+    const query = data.results[0]; // trivia object
     console.log(query);
-    const queryType = query.type;
-    question.innerHTML = query.question;
-    if (queryType == 'multiple') {
-      let answArr = [];
-      answArr = answArr.concat(query.correct_answer);
-      query.incorrect_answers.forEach((answer) => {
+    const queryType = query.type; // trivia type - true/false or multiple choice
+    question.innerHTML = query.question; // display trivia question in DOM
+    if (queryType == 'multiple') { // handle multiple choice
+      let answArr = []; // new array for all possible answers
+      answArr = answArr.concat(query.correct_answer); // add correct answer to array
+      query.incorrect_answers.forEach((answer) => { // add all incorrect answers to array
         answArr = answArr.concat(answer);
       });
       console.log(answArr);
+      // insert DOM elements for choices
+
+      answArr.forEach((answer) => {
+        // create new li & br elements
+        const li = document.createElement('li');
+        const br = document.createElement('br');
+        li.innerHTML = answer;
+        option.appendChild(li);
+        option.appendChild(br);
+      });
     }
   });
 }
